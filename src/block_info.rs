d4a9f5d2dda46d79b10261c3f16840782aae8b9f
KangarooFist <damienbonello97@gmail.com>
2022-05-19 23:18:28 +0200

    updates

---
+++ src/block_info.rs
@@ -0,0 +1,29 @@
+use dotenv;
+use reqwest;
+use tokio;
+use serde_json::Result;
+use crate::block_status::BlockStatus;
+use crate::block_address::BlockAddress;
+use crate::block_tx::BlockTx;
+
+const ROOT: &str = "https://eth-blockbook.nownodes.io/api/";
+
+#[tokio::main]
+pub async fn send_request(url: &str) -> String {
+
+    let client = reqwest::Client::new();
+    client
+        .get(url)
+        .header("api-key", dotenv::var("API_KEY").expect("Could not find API Key")) //the expect makes Rust throw out a panic.
+        .send()
+        .await
+        .expect("Failed - No response")
+        .text()
+        .await
+        .expect("Failed - Can't convert payload")
+}
+
+pub fn block_status_request() -> BlockStatus{
+    let response = send_request(ROOT);
+    serde_json::from_str(&response).expect("Failed to Parse")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+
+#[macro_use]
+extern crate serde_derive;
+
+mod block_info;
+mod block_address;
+mod block_status;
+mod block_tx;
+
+//mod brings in other files 
+
+use crate::block_status::BlockStatus;
+use crate::block_address::BlockAddress;
+use crate::block_tx::BlockTx;
+
 fn main() {
-    println!("Hello, world!");
+    let block_status: BlockStatus = block_info::block_status_request();
+    println!("\n\nQuerying {} - chain: {}\n\n", &block_status.blockbook.coin, &block_status.backend.chain);
 }
